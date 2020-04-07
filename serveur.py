3df3a231510000be79139346bbc35551b67b9464
Lens Hunnel <mbinalens@gmail.com>
2020-04-07 09:28:22 +0200

    Update serveur.py

--- serveur.py
+++ serveur.py
@@ -1,8 +1,12 @@
 # coding: utf8
 import web
+import models
+
+
 
 urls = (
-    '/move', 'forward',
+    '/move', 'move',
+    '/action', 'action',
 )
 app = web.application(urls, globals())
 
@@ -10,9 +14,33 @@ app = web.application(urls, globals())
 class forward:
     def GET(self):
         direction = web.ctx.query.split('=')[1]
-        print(repr(direction))
+        if direction.upper() == "F":
+            models.m1.clockwise()
+            models.m2.clockwise()
+        elif direction.upper() == "B":
+            models.m1.counter_clockwise()
+            models.m2.counter_clockwise()
+        elif direction.upper() == "R":
+            models.m1.counter_clockwise()
+            models.m2.clockwise()
+        elif direction.upper() == "L":
+            models.m1.clockwise()
+            models.m2.counter_clockwise()
+        elif direction.upper() == "S":
+            models.m1.stop()
+            models.m2.stop()
+        else:
+            print("movement non supporte")
         return 'OK'
-
+    
+class action:
+    def GET(self):
+        action = web.ctx.query.split('=')[1]
+        if action == "1":
+            models.u.getdistance()
+       else:
+            print("action non supportee")
+        return 'OK'
 
 if __name__ == "__main__":
     app.run()
